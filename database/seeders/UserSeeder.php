0b409f0f5f5958421cfb9cba86bcd092f28abf1f
Muhin <muhin@blubirdinteractive.com>
2023-11-26 22:32:15 +0600

    creating user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'super_admin@gmail.com',
+            'username' => 'super_admin',
+            'password' => Hash::make('123456'),
+            'role'  => 1,
+            'created_at'  => date('Y-m-d h:i:s')
+        ]);
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'username' => 'admin',
+            'password' => Hash::make('123456'),
+            'role'  => 2,
+            'created_at'  => date('Y-m-d h:i:s')
+        ]);
+    }
+}
