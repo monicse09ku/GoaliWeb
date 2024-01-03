1e40ff73c141b52b8951bd70aca91ed1a466f864
Muhin <muhin@blubirdinteractive.com>
2024-01-03 18:17:11 +0600

    Updating goal step migration file

--- database/migrations/2023_11_10_141014_create_goal_steps_table.php
+++ database/migrations/2023_11_10_141014_create_goal_steps_table.php
@@ -24,7 +24,7 @@ class CreateGoalStepsTable extends Migration
             $table->string('step_occurrence')->nullable();
             $table->string('step_occurrence_weekdays')->nullable();
             $table->text('attachments')->nullable();
-            $table->tinyInteger('is_complete')->default(0)->nullable();
+            $table->tinyInteger('is_complete')->default(0);
             $table->timestamps('completed_at')->nullable();
             $table->enum('status', ['active','inactive','deleted'])->default('active');
             $table->timestamps();
