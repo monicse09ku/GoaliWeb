64b1a683b0f005b1b1573e33d846846dc55336aa
Muhin <muhin@blubirdinteractive.com>
2024-02-12 18:46:07 +0600

    Adding step collaborators to step details api

--- app/Models/GoalStep.php
+++ app/Models/GoalStep.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class GoalStep extends Model
 {
     protected $guarded = [];
+
+    public function collaborators()
+    {
+        $instance = $this->hasMany('App\Models\StepCollaborator','step_id','id');
+        //$instance = $instance->select('clients.id','clients.first_name','clients.last_name');
+        $instance = $instance->leftJoin('clients','clients.id','=','step_collaborators.collaborator_id');
+        return $instance;
+    }
 }
